103c06de64dd9496d40dd754630aed4f40d321c1
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2023-07-02 21:22:21 +0530

    Added the school timetable data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { createContext, useContext, useState } from "react";
 import "./App.css";
+import ResponseData from "./data.json";
+
+const SlotContext = createContext(null);
+
+const url = "https://crossorigin.me" + encodeURIComponent("https://fountainheadschool.edupage.org/timetable/server/regulartt.js?__func=regularttGetData");
+
+const DBTable = ResponseData.r.dbiAccessorRes.tables;
+
+function Search() {
+	const { slots, setSlots } = useContext(SlotContext);
+
+	console.log(DBTable);
+}
 
 function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	const [slots, setSlots] = useState([]);
+	return (
+		<SlotContext.Provider value={{ slots, setSlots }}>
+			<h1 className="text-3xl font-bold underline">A simple Timetable generator for school</h1>;
+		</SlotContext.Provider>
+	);
 }
 
 export default App;
